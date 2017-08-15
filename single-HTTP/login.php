87c82dcd91f952d35477deb61a9242a23fa611a6
Elliott Sobek <sobeke@mymacewan.ca>
2017-08-14 23:07:25 -0600

    Create login page

---
+++ single-HTTP/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+		<meta charset="utf-8"></meta>
+	</head>
+	<body>
+		<div>
+			<h1>Test</h1>
+		</div>
+		<?php
+			echo "TEST";
+		?>
+	</body>
+	<footer>
+		<a href="index.html">Home</a>
+	</footer>
+</html>
