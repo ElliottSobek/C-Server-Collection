5579753f8dbfad036118a069b79ade63df7896e1
Elliott Sobek <elliottsobek@gmail.com>
2018-05-14 20:46:57 -0600

    Move all testing resources to dedicated dir

---
+++ tests/singleCon.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+singleConnectionTest() {
+	if [ $# -ne 2 ]; then
+		echo "Usage: . singleConnectionTest <dir_name> <integer>"
+		exit 1
+	fi
+	local cdw=$PWD
+
+	cd $1
+	for ((i = 0; i < $2; i++)); do
+		while IFS='' read -r line || [[ -n "$line" ]]; do
+    		echo $line | nc localhost 8888
+		done < getQuery.txt
+	done
+	IFS=' '
+
+	cd $cdw
+	return 0
+}
+
+singleConnectionTest "$@"
