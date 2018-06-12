da46643a5192fef16fdd5934a3dfd4b86466822f
Elliott Sobek <elliottsobek@gmail.com>
2018-06-11 20:28:59 -0600

    Add initial Binary search tree implementation

---
+++ single-HTTP/lib/bst/bst.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "bst.h"
+
+// var == NULL; EQ to: !var
+
+#define NT_LEN 1
+#define STR_MAX 2048
+
+static Bst_Node create_node(const String const regex, const String const path) {
+	Bst_Node const node = (Bst_Node) malloc(sizeof(bst_node_t));
+
+	if (!node)
+		exit(EXIT_FAILURE);
+
+	const size_t regex_len = strnlen(regex, STR_MAX);
+	node->regex = (String) calloc((regex_len + NT_LEN), sizeof(char));
+
+	if (!node->regex)
+		exit(EXIT_FAILURE);
+
+	strncpy(node->regex, regex, regex_len);
+	const size_t path_len = strnlen(path, STR_MAX);
+	node->path = (String) calloc((path_len + NT_LEN), sizeof(char));
+
+	if (!node->path)
+		exit(EXIT_FAILURE);
+
+	strncpy(node->path, path, path_len);
+
+	node->left = NULL;
+	node->right = NULL;
+
+	return node;
+}
+
+static void bst_destroy_tree(Bst_Node root) {
+	if (!root)
+		return;
+
+	bst_destroy_tree(root->left);
+	bst_destroy_tree(root->right);
+	free(root->regex);
+	root->regex = NULL;
+
+	free(root->path);
+	root->path = NULL;
+
+	free(root);
+	root = NULL;
+}
+
+void bst_print_in_order(Bst_Node const root) {
+	if (!root)
+		return;
+	bst_print_in_order(root->left);
+	printf("%s\n%s\n;", root->regex, root->path);
+	bst_print_in_order(root->right);
+}
+
+void bst_insert(Bst bst, const String const regex, const String const path) {
+	Bst_Node const new_node = create_node(regex, path);
+
+	if (!bst->root) {
+		bst->root = new_node;
+		return;
+	}
+	int cmp_res;
+	Bst_Node cur = bst->root, parent;
+
+	while (cur) {
+		cmp_res = strncmp(path, cur->path, strnlen(path, STR_MAX));
+		parent = cur;
+
+		if (cmp_res < 0)
+			cur = cur->left;
+		else
+			cur = cur->right;
+	}
+
+	if (cmp_res < 0)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
+}
+
+void bst_destroy(Bst bst) {
+	bst_destroy_tree(bst->root);
+	free(bst);
+	bst = NULL;
+}
+
+Bst bst_create(void) {
+	Bst bst = (Bst) malloc(sizeof(bst_t));
+
+	if (!bst)
+		exit(EXIT_FAILURE);
+	bst->root = (Bst_Node) malloc(sizeof(bst_node_t));
+
+	if (!bst->root)
+		exit(EXIT_FAILURE);
+
+	return bst;
+}
---
+++ single-HTTP/lib/bst/bst.h
@@ -0,0 +1,25 @@
+#ifndef BST_H
+#define BST_H
+
+#include "../types/types.h"
+
+typedef struct bst_node_s {
+	String regex, path;
+	struct bst_node_s *left, *right;
+} bst_node_t;
+
+typedef bst_node_t *Bst_Node;
+
+typedef struct bst_s {
+	Bst_Node root;
+} bst_t;
+
+typedef bst_t *Bst;
+
+extern Bst bst_create(void);
+extern void bst_destroy(Bst);
+extern void bst_insert(Bst, const String const, const String const);
+extern String bst_get_value(Bst_Node const, const String const);
+extern void bst_print_in_order(Bst_Node const);
+
+#endif /* End BST_H */
