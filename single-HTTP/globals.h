96faaf19585cae8ba8c47c8b189eceb65419334b
Elliott Sobek <elliottsobek@gmail.com>
2018-08-02 22:58:23 -0600

    Add debug macros

--- single-HTTP/globals.h
+++ single-HTTP/globals.h
@@ -1,6 +1,7 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <linux/limits.h>
@@ -10,6 +11,10 @@
 #define MBYTE_S ((uint64_t) (KBYTE_S * KBYTE_S))
 
 #define NT_LEN 1
+
+#define _debug_string(x) (printf("__Debug__ File: %s, Function: %s, Line: %d; %s\n", __FILE__, __func__, __LINE__, (x)))
+#define _debug_int(x) (printf("__Debug__ File: %s, Function: %s, Line: %d; %lli\n", __FILE__, __func__, __LINE__, (x)))
+#define _debug_char(x) (printf("__Debug__ File: %s, Function: %s, Line: %d; %c\n", __FILE__, __func__, __LINE__, (x)))
 
 bool verbose_flag;
 char _log_root[PATH_MAX];
