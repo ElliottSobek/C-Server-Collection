210568b937e51dfd8bd6534471d6861d9f783215
Elliott Sobek <elliottsobek@gmail.com>
2018-08-03 19:05:01 -0600

    Implement dynamic paramaterizable query logic

--- single-HTTP/lib/sqlite3/sqlite3.c
+++ single-HTTP/lib/sqlite3/sqlite3.c
@@ -21,7 +21,7 @@ typedef struct query_s {
 
 typedef quert_t *Query;
 
-static Query parse_stmt(String stmt) {
+static Query parse_stmt(const String const restrict stmt) {
     const size_t prepare_stmt_len = strnlen(stmt, KBYTE_S * 2);
     char prepare_stmt[(KBYTE_S * 2) + NT_LEN], result[(KBYTE_S * 2) + NT_LEN] = "", specifiers[63 + NT_LEN] = "";
 
@@ -36,8 +36,6 @@ static Query parse_stmt(String stmt) {
         } else
             result[j] = prepare_stmt[i];
     }
-
-    _debug_string(stmt);
 
     Query query = (Query) malloc(sizeof(quert_t));
 
@@ -56,8 +54,6 @@ static Query parse_stmt(String stmt) {
 
     if (!query->specifiers)
         exit(EXIT_FAILURE);
-
-    _debug_string(specifiers);
 
     if (specifiers[0] != '\0') {
         query->is_parameterized = true;
@@ -82,18 +78,6 @@ static void destroy_query(Query query) {
     query = NULL;
     return;
 }
-
-// static int result_set(void *unused, int col_num, char **row_value, char **col_name) {
-//     if (!verbose_flag)
-//         return -1;
-
-//     printf("| %s |", row_value[0] ? row_value[0]: "NULL");
-
-//     for (int i = 1; i < col_num; i++)
-//         printf(" %s |", row_value[i] ? row_value[i]: "NULL");
-//     printf("\n");
-//     return 0;
-// }
 
 static void print_headers(const int rows, sqlite3_stmt *sql_byte_code) {
     sqlite3_step(sql_byte_code);
@@ -124,7 +108,7 @@ static void print_rows(const int rows, sqlite3_stmt *sql_byte_code) {
     } while(result == SQLITE_ROW);
 }
 
-int select_debug(const String const restrict stmt) {
+int sqlite_exec(const String const restrict stmt, ...) {
     sqlite3 *db;
     sqlite3_stmt *sql_byte_code;
     int result_code = sqlite3_open("database/db.sqlite3", &db);
@@ -133,7 +117,8 @@ int select_debug(const String const restrict stmt) {
         fprintf(stderr, RED "Database Error: Cannot open database: %s\n" RESET, sqlite3_errmsg(db));
         exit(EXIT_FAILURE);
     }
-    result_code = sqlite3_prepare_v2(db, stmt, KBYTE_S * 2, &sql_byte_code, NULL);
+    const Query const restrict query = parse_stmt(stmt);
+    result_code = sqlite3_prepare_v2(db, query->stmt, KBYTE_S * 2, &sql_byte_code, NULL);
 
     if (result_code != SQLITE_OK) {
         if (verbose_flag)
@@ -142,95 +127,55 @@ int select_debug(const String const restrict stmt) {
         sqlite3_close(db);
         return -1;
     }
-    const int rows = sqlite3_column_count(sql_byte_code);
 
-    print_headers(rows, sql_byte_code);
-    print_rows(rows, sql_byte_code);
+    if (query->is_parameterized) {
+        char c;
+        va_list args;
+        struct stat file;
+        String string;
+
+        va_start(args, stmt);
+
+        for (unsigned short i = 0; i < query->specifiers_len; i++) {
+            c = query->specifiers[i];
+
+            switch (c) {
+            case 'd':
+                sqlite3_bind_int(sql_byte_code, i + 1, va_arg(args, int));
+                continue;
+            case 's':
+                string = va_arg(args, char*);
+
+                sqlite3_bind_text(sql_byte_code, i + 1, string, strnlen(string, PATH_MAX), SQLITE_STATIC);
+                continue;
+            case 'f':
+                sqlite3_bind_double(sql_byte_code, i + 1, va_arg(args, double));
+                continue;
+            case 'b':
+                string = va_arg(args, char*);
+
+                stat(string, &file);
+                sqlite3_bind_blob(sql_byte_code, i + 1, string, file.st_size, SQLITE_STATIC);
+                continue;
+            }
+        }
+        va_end(args);
+    }
+
+    if (strncasecmp("SELECT", query->stmt, 6) == 0) {
+        const int rows = sqlite3_column_count(sql_byte_code);
+
+        print_headers(rows, sql_byte_code);
+        print_rows(rows, sql_byte_code);
+    } else {
+        sqlite3_step(sql_byte_code);
+
+        if (verbose_flag)
+            printf("Rows affected: %d\n", sqlite3_changes(db));
+    }
+    destroy_query(query);
     sqlite3_finalize(sql_byte_code);
     sqlite3_close(db);
 
     return 0;
-}
-
-int sqlite_exec(const String const restrict stmt, ...) {
-    // sqlite3 *db;
-    // sqlite3_stmt *sql_byte_code;
-    // int result_code = sqlite3_open("database/db.sqlite3", &db);
-
-    // if (result_code != SQLITE_OK) {
-    //     fprintf(stderr, RED "Database Error: Cannot open database: %s\n" RESET, sqlite3_errmsg(db));
-    //     exit(EXIT_FAILURE);
-    // }
-    // result_code = sqlite3_prepare_v2(db, stmt, KBYTE_S * 2, &sql_byte_code, NULL);
-
-    // if (result_code != SQLITE_OK) {
-    //     if (verbose_flag)
-    //         fprintf(stderr, YELLOW "SQL error: %s\n" RESET, sqlite3_errmsg(db));
-    //     sqlite3_finalize(sql_byte_code);
-    //     sqlite3_close(db);
-    //     return -1;
-    // }
-    // const int rows = sqlite3_column_count(sql_byte_code);
-    // printf("%d\n", rows);
-    Query query = parse_stmt(stmt);
-
-    printf("query->stmt: %s\n", query->stmt);
-    printf("query->specifiers: %s\n", query->specifiers);
-    printf("query->specifiers_len: %lu\n", query->specifiers_len);
-    printf("query->is_parameterized: %s\n", query->is_parameterized ? "True" : "False");
-
-    destroy_query(query);
-    // sqlite3_finalize(sql_byte_code);
-    // sqlite3_close(db);
-
-    return 0;
-
-    // if (query->is_parameterized) {
-    //     char c;
-    //     va_list args;
-    //     struct stat file;
-    //     String string;
-
-    //     va_start(args, stmt);
-
-    //     for (unsigned short i = 0; i < query->specifiers_len; i++) {
-    //         c = query->specifiers[i];
-
-    //         switch (c) {
-    //         case 'd':
-    //             sqlite3_bind_int(sql_byte_code, i + 1, va_arg(args, int));
-    //             continue;
-    //         case 's':
-    //             string = va_arg(args, char*);
-
-    //             sqlite3_bind_text(sql_byte_code, i + 1, string, strnlen(string, PATH_MAX), SQLITE_STATIC);
-    //             continue;
-    //         case 'f':
-    //             sqlite3_bind_double(sql_byte_code, i + 1, va_arg(args, double));
-    //             continue;
-    //         case 'b':
-    //             string = va_arg(args, char*);
-
-    //             stat(string, &file);
-    //             sqlite3_bind_blob(sql_byte_code, i + 1, va_arg(args, char*), file.st_size, SQLITE_STATIC);
-    //             continue;
-    //         }
-    //     }
-    //     va_end(args);
-    // }
-
-    // if (strncasecmp("SELECT", query->stmt, 6) == 0) {
-    //     print_headers(rows, sql_byte_code);
-    //     print_rows(rows, sql_byte_code);
-    // } else {
-    //     sqlite3_step(sql_byte_code);
-
-    //     if (verbose_flag)
-    //         printf("Rows affected: %d\n", sqlite3_changes(db));
-    // }
-    // destroy_query(query);
-    // sqlite3_finalize(sql_byte_code);
-    // sqlite3_close(db);
-
-    // return 0;
 }
