5d04d7816d9938715a8f9f37f428749989365259
Elliott Sobek <elliottsobek@gmail.com>
2018-08-28 20:39:29 -0600

    Implement database table dump feature

--- single-HTTP/lib/sqlite3/sqlite3.c
+++ single-HTTP/lib/sqlite3/sqlite3.c
@@ -232,7 +232,93 @@ void sqlite_dumpdb(void) {
 }
 
 void sqlite_dumptable(const String restrict table) {
-    puts("Dump table");
+    sqlite3 *db;
+    sqlite3_stmt *stmt_table, *stmt_data;
+    const char *table_name, *data;
+    int col_cnt = 0, ret = 0;
+    char cmd[4096] = {0};
+
+    ret = sqlite3_open(_db_path, &db);
+
+    if (ret != SQLITE_OK) {
+        fprintf(stderr, RED "Database Error: Cannot open database: %s\n" RESET, sqlite3_errmsg(db));
+        exit(EXIT_FAILURE);
+    }
+
+    ret = sqlite3_prepare_v2(db, "SELECT sql, tbl_name FROM sqlite_master WHERE type = 'table'", -1, &stmt_table, NULL);
+
+    if (ret != SQLITE_OK)
+        return;
+
+    printf("PRAGMA foreign_keys=off;\nBEGIN TRANSACTION;\n");
+
+    ret = sqlite3_step(stmt_table);
+
+    while (ret == SQLITE_ROW) {
+        data = (char*) sqlite3_column_text(stmt_table, 0);
+        table_name = (char*) sqlite3_column_text(stmt_table, 1);
+
+        if (!data || !table_name)
+            return;
+
+        printf("%s;\n", data);
+        sprintf(cmd, "SELECT * FROM %s;", table_name);
+
+        ret = sqlite3_prepare_v2(db, cmd, -1, &stmt_data, NULL);
+
+        if (ret != SQLITE_OK)
+            return;
+
+        ret = sqlite3_step(stmt_data);
+
+        while (ret == SQLITE_ROW) {
+            sprintf(cmd, "INSERT INTO \"%s\" VALUES (", table_name);
+            col_cnt = sqlite3_column_count(stmt_data);
+
+            for (int index = 0; index < col_cnt; index++) {
+                if (index)
+                    strcat(cmd, ",");
+                data = (char*) sqlite3_column_text(stmt_data, index);
+
+                if (data) {
+                    if (sqlite3_column_type(stmt_data, index) == SQLITE_TEXT) {
+                        strcat(cmd, "'");
+                        strcat(cmd, data);
+                        strcat(cmd, "'");
+                    } else
+                        strcat(cmd, data);
+                } else
+                    strcat(cmd, "NULL");
+            }
+            printf("%s);\n", cmd);
+            ret = sqlite3_step(stmt_data);
+        }
+        ret = sqlite3_step(stmt_table);
+    }
+
+    if (stmt_table)
+        sqlite3_finalize(stmt_table);
+
+    ret = sqlite3_prepare_v2(db, "SELECT sql FROM sqlite_master WHERE type = 'trigger';", -1, &stmt_table, NULL);
+
+    if (ret != SQLITE_OK)
+        return;
+
+    ret = sqlite3_step(stmt_table);
+
+    while (ret == SQLITE_ROW) {
+        data = (char*) sqlite3_column_text(stmt_table, 0);
+
+        if (!data)
+            return;
+
+        printf("%s;\n", data);
+
+        ret = sqlite3_step(stmt_table);
+    }
+
+    printf("COMMIT;\n");
+
     return;
 }
 
