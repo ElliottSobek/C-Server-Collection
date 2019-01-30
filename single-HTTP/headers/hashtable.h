b4af42ad8851e5ac647e0d5557d1120ee3c0ab5f
Elliott Sobek <elliottsobek@gmail.com>
2019-01-29 22:32:10 -0700

    Remove redundant struct names ^ fix naming

--- single-HTTP/headers/hashtable.h
+++ single-HTTP/headers/hashtable.h
@@ -6,19 +6,19 @@
 typedef struct ll_node_s {
 	String key;
 	Generic value;
-	Data_Type dt;
+	DataType dt;
 	struct ll_node_s *next;
 } ll_node_t;
 
 typedef ll_node_t *Ll_Node;
 
-typedef struct ll_s {
+typedef struct {
 	Ll_Node root;
 } ll_t;
 
 typedef ll_t *Ll;
 
-typedef struct hashtable_s {
+typedef struct {
 	Ll *bins;
 	unsigned int max_size, cur_size;
 } hashtable_t;
@@ -27,7 +27,7 @@ typedef hashtable_t *HashTable;
 
 extern HashTable ht_create(const unsigned int);
 extern void ht_destroy(HashTable);
-extern void ht_insert(HashTable *const, const String, const Generic, const Data_Type);
+extern void ht_insert(HashTable *const, const String, const Generic, const DataType);
 extern void ht_remove(const HashTable, const String);
 extern Generic ht_get_value(HashTable const, const String);
 extern void ht_print(HashTable const);
