03c379a4a3472d7ce37738420efe48aae86aab2b
Elliott Sobek <elliottsobek@gmail.com>
2019-01-30 14:58:09 -0700

    Enable specific file extension searching

--- single-HTTP/getpaths.sh
+++ single-HTTP/getpaths.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 getPaths() {
-	if [ "$#" -lt 1 ]; then
-		echo "Usage:" `basename "$0"` "<dir_names>"
+	if [ "$#" -lt 2 ]; then
+		echo "Usage:" `basename "$0"` "<file_ext> <dir_names>"
 		exit 1
 	fi
+	local extension=*."$1"
+
+	shift
 
 	local relative_wds=()
 
 	for arg in "$@"; do
 
 		if [ -d $arg ]; then
-			local dirlist=`find -O3 $arg -type f -name "*.c" -printf "%h\n" | uniq`
+			local dirlist=`find -O3 $arg -type f -name "$extension" -printf "%h\n" | uniq`
 
 			for dir in $dirlist; do
 				relative_wds+="$dir "
