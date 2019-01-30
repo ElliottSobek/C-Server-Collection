2714f8200ea6bb16124dcf0573c6c4b9d36be27b
Elliott Sobek <elliottsobek@gmail.com>
2019-01-29 22:33:02 -0700

    Remove redundant struct names & fix naming

--- single-HTTP/headers/s_linked_list.h
+++ single-HTTP/headers/s_linked_list.h
@@ -4,13 +4,14 @@
 #include "types.h"
 
 typedef struct s_ll_node_s {
-	String regex, path;
+	Generic value;
+	DataType dt;
 	struct s_ll_node_s *next;
 } s_ll_node_t;
 
 typedef s_ll_node_t *S_Ll_Node;
 
-typedef struct s_ll_s {
+typedef struct {
 	S_Ll_Node root;
 } s_ll_t;
 
@@ -18,8 +19,8 @@ typedef s_ll_t *S_Ll;
 
 extern S_Ll s_ll_create(void);
 extern void s_ll_destroy(S_Ll);
-extern void s_ll_insert(S_Ll, const String, const String);
-extern S_Ll_Node s_ll_find(S_Ll const, const String);
+extern void s_ll_insert(S_Ll, const Generic, const DataType);
+// extern S_Ll_Node s_ll_find(S_Ll const, const Generic);
 extern void s_ll_print(S_Ll);
 
 #endif /* End S_LINKED_LIST_H */
