35f5281fdd27ccbd48ba30a05726813e426baf10
Elliott Sobek <elliottsobek@gmail.com>
2019-01-30 19:12:58 -0700

    Format & refactor to return NULL on error

--- single-HTTP/src/sqlite/resultset.c
+++ single-HTTP/src/sqlite/resultset.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,23 +11,32 @@
 ResultSet create_rs(const int rows, String *column_names, S_Ll row_data, unsigned long result_amount) {
     const ResultSet rs = (ResultSet) malloc(sizeof(resultset_t));
 
-    if (!rs) {
-        fprintf(stderr, "Memory Error: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
+    if (!rs)
+        return NULL;
     rs->column_names = (String*) malloc(rows * sizeof(String));
 
     if (!rs->column_names) {
-        fprintf(stderr, "Memory Error: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
+        free(rs);
+        rs = NULL;
+
+        return NULL;
     }
 
     for (int i = 0; i < rows; i++) {
         rs->column_names[i] = (String) calloc(strnlen(column_names[i], STR_MAX) + NT_LEN, sizeof(char));
 
         if (!rs->column_names[i]) {
-            fprintf(stderr, "Memory Error: %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
+            free(rs);
+            rs = NULL;
+
+            free(rs->column_names);
+            rs->column_names = NULL;
+
+            for (int j = 0; j < i; j++) {
+                free(rs->column_names[j]);
+                rs->column_names[j] = NULL;
+            }
+            return NULL;
         }
     }
     rs->query_result = row_data;
@@ -36,6 +44,39 @@ ResultSet create_rs(const int rows, String *column_names, S_Ll row_data, unsigne
     rs->result_amount = result_amount;
 
     return rs;
+}
+
+ResultSet parse_query_result(const int rows, sqlite3_stmt *sql_byte_code) {
+    String column_names[rows];
+
+    sqlite3_step(sql_byte_code);
+
+    for (int i = 0; i < rows; i++)
+        column_names[i] = (String) sqlite3_column_name(sql_byte_code, i);
+
+    unsigned long result_amount = 0;
+    String row_data[rows + NT_LEN], row_datum;
+    S_Ll result_rows = s_ll_create();
+
+    if (!result_rows)
+        return NULL;
+
+    while (sqlite3_step(sql_byte_code) == SQLITE_ROW) {
+        for (int i = 0; i < rows; i++) {
+            row_data[i] = "";
+            row_datum = (String) sqlite3_column_text(sql_byte_code, i);
+
+            if (!row_data)
+                row_data[i] = "NULL";
+            else
+                row_data[i] = row_datum;
+        }
+        row_data[rows + NT_LEN] = NULL;
+
+        s_ll_insert(result_rows, (Generic) row_data, STRING_ARRAY); // Check return value?
+        result_amount++;
+    }
+    return create_rs(rows, column_names, result_rows, result_amount);
 }
 
 void sqlite_destroy_rs(ResultSet rs) {
@@ -53,39 +94,6 @@ void sqlite_destroy_rs(ResultSet rs) {
     rs = NULL;
 
     return;
-}
-
-ResultSet parse_query_result(const int rows, sqlite3_stmt *sql_byte_code) {
-    if (rows < 1)
-        return NULL;
-    String column_names[rows];
-
-    sqlite3_step(sql_byte_code);
-
-    for (int i = 0; i < rows; i++)
-        column_names[i] = (String) sqlite3_column_name(sql_byte_code, i);
-
-    unsigned long result_amount = 0;
-    String row_data[rows + NT_LEN], row_datum;
-    S_Ll result_rows = s_ll_create();
-
-    if (!result_rows) {
-        fprintf(stderr, "Memory Error: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-
-    while (sqlite3_step(sql_byte_code) == SQLITE_ROW) {
-        for (int i = 0; i < rows; i++) {
-            row_data[i] = "";
-            row_datum = (String) sqlite3_column_text(sql_byte_code, i);
-            row_data[i] = row_datum ? row_datum: "NULL";
-            printf("This is row_data[i]: %s\n", row_data[i]);
-        }
-        row_data[rows + NT_LEN] = NULL;
-        s_ll_insert(result_rows, (Generic) row_data, STRING_ARRAY);
-        result_amount++;
-    }
-    return create_rs(rows, column_names, result_rows, result_amount);
 }
 
 void sqlite_print_rs(ResultSet rs) {
